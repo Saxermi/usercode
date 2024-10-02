e225dc5e48a5e73efbb401e73a6b8d24a81f3c39
saxermi <137685346+Saxermi@users.noreply.github.com>
2024-10-02 12:16:15 +0200

    another try

--- PrimaryVertexAnalyzer/src/TestAnalyzer.cc
+++ PrimaryVertexAnalyzer/src/TestAnalyzer.cc
@@ -368,8 +368,14 @@ std::map<std::string, TH1*> TestAnalyzer::bookVertexHistograms(TDirectory * dir)
     // Definition of the 2D histogram
     TH2F* PUhist_reco_vs_true_z_position = new TH2F("PUreco_vs_true_z_position", "PU Reconstructed vs. True Z-Position", 100, -10, 10, 100, -10, 10);
     addn(h, PUhist_reco_vs_true_z_position);
-    PUhist_reco_vs_true_z_position->Draw("COLZ");
+    PUhist_reco_vs_true_z_position->Draw("COLZ"); // should add a legend, doesent work tough might remove or find alternative.
+// Create and customize the legend, positioned on the left side
+    TLegend* legend = new TLegend(0.15, 0.75, 0.35, 0.85);  // Adjust position as needed to avoid overlap
+    legend->AddEntry(PUhist_reco_vs_true_z_position, "Reco vs True Z", "");
+    legend->Draw();
 
+// Directly update the current histogram's pad to display everything
+gPad->Update();
     //new histogram
     //definition of 1 H hist
     TH1F *SEHistRecoVsTrueZPositionHist = new TH1F("SE_reco_vs_true_z_position_hist","SE Reconstructed vs. True Z-Position position difference", 200, -0.01, 0.01);
