9710806ca6790d123626a2cb3d9880db731dac00
saxermi <137685346+Saxermi@users.noreply.github.com>
2024-12-10 11:02:04 +0100

    submitted exp 21

--- PrimaryVertexAnalyzer/test/experiment_master.py
+++ PrimaryVertexAnalyzer/test/experiment_master.py
@@ -10,7 +10,7 @@ TEST_MODE = False  # Set to False to submit all jobs
 
 # Get the current timestamp for the log filename
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-base_path = "experimental_run_20"   stop
+base_path = "experimental_run_21"   
 log_filename = f"{base_path}_{timestamp}.log"
 log_path = os.path.join("/work/msaxer", log_filename)
 
@@ -65,7 +65,7 @@ def submit_job(sample, overlap, blocksize, iterating_blocksize=False, notify=Fal
     overlap_dir_name = f"n{overlap_dir}" if overlap < 0 else f"{overlap_dir}"
 
     # Hardcoded base path for experimental runs
-    base_path = "experimental_run_20"
+    base_path = "experimental_run_21"
     dir_create_path = "/work/msaxer/"
 
     # Create the full path based on the sample, overlap, and blocksize
@@ -124,22 +124,22 @@ def main():
     # Samples to be used (from TTbar_01 to TTbar_15)
     # samples = [f"TTbar_{str(i).zfill(2)}" for i in range(1, 16)]
     
-    #subsets = [
-     #   "Subset_SToMuMu_01", "Subset_ZMM_03", "Subset_SToMuMu_02", 
-      #  "Subset_TTbar_01", "Subset_TTbar_02", "Subset_HiggsGluonFusion_01", 
-      #  "Subset_HiggsGluonFusion_02", "Subset_ZMM_01", "Subset_ZMM_02"
-    #]
     subsets = [
-        
-        "Subset_TTbar_01", "Subset_TTbar_02"
-        
+        "Subset_SToMuMu_01", "Subset_ZMM_03", "Subset_SToMuMu_02", 
+        "Subset_TTbar_01", "Subset_TTbar_02", "Subset_HiggsGluonFusion_01", 
+        "Subset_HiggsGluonFusion_02", "Subset_ZMM_01", "Subset_ZMM_02"
     ]
+    #subsets = [
+        
+    #    "Subset_TTbar_01", "Subset_TTbar_02"
+        
+    #]
     #subsets = [
      #   "Subset_ZMM_02"
     #]
     #subsets = ["Subset_SToMuMu_01"]
     # Overlap values from 0.0 to 0.9 in 0.1 increments (include negative values if needed)
-    overlaps = [0,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75]  # Add negative overlaps here
+    overlaps = [0.4]  # Add negative overlaps here
 
     # Block sizes to iterate over
     blocksizes = [512]
