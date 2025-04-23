b1012ae3401a7ffb29ade00c10f1b1c9d33a2b9e
Freja Rasmussen <frejalrasmussen@gmail.com>
2025-04-23 12:07:51 +0200

    run 65, beta1= 0.25, global Da back

--- PrimaryVertexAnalyzer/test/experiment_master_frejalom.py
+++ PrimaryVertexAnalyzer/test/experiment_master_frejalom.py
@@ -10,7 +10,7 @@ TEST_MODE = False  # Set to False to submit all jobs
 global base_path
 # Get the current timestamp for the log filename
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-base_path = "experimental_run_63"
+base_path = "experimental_run_65"
 log_filename = f"{base_path}_{timestamp}.log"
 log_path = os.path.join("/work/frejalom/ba", log_filename)
 
